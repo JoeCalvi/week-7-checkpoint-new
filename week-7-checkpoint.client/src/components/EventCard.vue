21c0e2cbcdc975294340286c74bdc67bc0f80418
JoeCalvi <joecalvi@me.com>
2023-03-01 13:15:33 -0700

    eventCard component working

---
+++ week-7-checkpoint.client/src/components/EventCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="EventCard">
+        <router-link :to="{ name: 'Event', params: { eventId: event.id } }" class="text-dark">
+            <div class="card event-card p-3 selectable">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h4 class="p-2">{{ event.name }}</h4>
+                        <div class="row align-items-center">
+                            <div class="col-6">
+                                <img :src="event.coverImg" :alt="event.coverImg" class="img-fluid rounded">
+                            </div>
+                            <div class="col-6">
+                                <div class="card-body border border-dark rounded">
+                                    <ul>
+                                        <li>{{ event.startDate }}</li>
+                                        <li>Capacity: {{ event.capacity }}</li>
+                                        <li>{{ event.location }}</li>
+                                        <li>Event Type: {{ event.type }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+
+<script>
+
+import { Event } from "../models/Event.js";
+export default {
+    props: {
+        event: {
+            type: Event,
+            required: true
+        }
+    },
+    setup() {
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.event-card {
+    height: 45vh;
+}
+</style>
--- week-7-checkpoint.client/src/pages/HomePage.vue
+++ week-7-checkpoint.client/src/pages/HomePage.vue
@@ -4,30 +4,7 @@
       <div class="col-md-10">
         <div class="row">
           <div v-for="e in events" class="col-6 mb-3">
-            <router-link :to="{ name: 'Event', params: { eventId: e.id } }" class="text-dark">
-              <div class="card event-card p-3 selectable">
-                <div class="row">
-                  <div class="col-12 text-center">
-                    <h4 class="p-2">{{ e.name }}</h4>
-                  </div>
-                  <div class="row align-items-center">
-                    <div class="col-6">
-                      <img :src="e.coverImg" :alt="e.coverImg" class="img-fluid rounded">
-                    </div>
-                    <div class="col-6">
-                      <div class="card-body border border-dark rounded">
-                        <ul>
-                          <li>{{ e.startDate }}</li>
-                          <li>Capacity: {{ e.capacity }}</li>
-                          <li>{{ e.location }}</li>
-                          <li>Event Type: {{ e.type }}</li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </router-link>
+            <EventCard :event="e" />
           </div>
         </div>
       </div>
@@ -41,31 +18,30 @@ import Pop from '../utils/Pop.js';
 import { eventsService } from "../services/EventsService.js";
 import { AppState } from '../AppState.js';
 import { useRoute } from 'vue-router';
+import EventCard from '../components/EventCard.vue';
 
 export default {
   setup() {
-    const route = useRoute()
-
+    const route = useRoute();
     async function getAllEvents() {
       try {
         await eventsService.getAllEvents();
-      } catch (error) {
-        Pop.error('[GETTING ALL EVENTS]', error);
+      }
+      catch (error) {
+        Pop.error("[GETTING ALL EVENTS]", error);
       }
     }
-
     onMounted(() => {
-      getAllEvents()
-    })
+      getAllEvents();
+    });
     return {
       events: computed(() => AppState.events),
-    }
-  }
+    };
+  },
+  components: { EventCard }
 }
 </script>
 
 <style scoped lang="scss">
-.event-card {
-  height: 45vh;
-}
+
 </style>
