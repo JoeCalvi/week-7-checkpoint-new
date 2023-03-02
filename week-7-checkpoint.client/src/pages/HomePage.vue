5c7fc06482f3cf39931dc20e0cc7f15ab345c2c6
JoeCalvi <joecalvi@me.com>
2023-03-01 21:43:44 -0700

    can filter events on home page

--- week-7-checkpoint.client/src/pages/HomePage.vue
+++ week-7-checkpoint.client/src/pages/HomePage.vue
@@ -2,6 +2,15 @@
   <div class="container-fluid">
     <div class="row justify-content-center my-3">
       <div class="col-md-10">
+        <div class="row my-3">
+          <div class="col-12 d-flex justify-content-evenly bg-dark rounded p-3">
+            <button class="btn btn-warning" @click="changeFilterType('')">All</button>
+            <button class="btn btn-warning" @click="changeFilterType('concert')">Concert</button>
+            <button class="btn btn-warning" @click="changeFilterType('convention')">Convention</button>
+            <button class="btn btn-warning" @click="changeFilterType('sport')">Sport</button>
+            <button class="btn btn-warning" @click="changeFilterType('digital')">Digital</button>
+          </div>
+        </div>
         <div class="row">
           <div v-for="e in events" class="col-6 mb-3">
             <EventCard :event="e" />
@@ -13,16 +22,15 @@
 </template>
 
 <script>
-import { onMounted, computed } from 'vue';
+import { onMounted, computed, ref } from 'vue';
 import Pop from '../utils/Pop.js';
 import { eventsService } from "../services/EventsService.js";
 import { AppState } from '../AppState.js';
-import { useRoute } from 'vue-router';
 import EventCard from '../components/EventCard.vue';
 
 export default {
   setup() {
-    const route = useRoute();
+    const filterEvents = ref('')
     async function getAllEvents() {
       try {
         await eventsService.getAllEvents();
@@ -31,11 +39,20 @@ export default {
         Pop.error("[GETTING ALL EVENTS]", error);
       }
     }
-    onMounted(() => {
-      getAllEvents();
-    });
+    onMounted(() =>
+      getAllEvents()
+    );
     return {
-      events: computed(() => AppState.events),
+      events: computed(() => {
+        if (!filterEvents.value) {
+          return AppState.events
+        } else {
+          return AppState.events.filter(e => e.type == filterEvents.value)
+        }
+      }),
+      changeFilterType(type) {
+        filterEvents.value = type
+      }
     };
   },
   components: { EventCard }
